09cb399e57980a118d8789dd069ef8fc7ca4ece8
Undersin <andersonvieira818@yahoo.com.br>
2022-11-27 08:06:49 -0300

    Iniciando sessão de usuário

---
+++ ContactControl/Helpers/ISession.cs
@@ -0,0 +1,11 @@
+﻿using ContactControl.Models;
+
+namespace ContactControl.Helpers;
+
+// interface para definir o contrado das sessões do usuário
+public interface ISession
+{
+    void CreateUserSession(UserModel user);
+    void RemoveUserSession();
+    UserModel GetUserSession();
+}
---
+++ ContactControl/Helpers/Session.cs
@@ -0,0 +1,35 @@
+﻿using ContactControl.Models;
+using System.Text.Json;
+
+namespace ContactControl.Helpers;
+
+public class Session : ISession
+{
+    // inteface do aspnet. Essa interface vai permitir o armazenamento da sessão do usuário
+    private readonly IHttpContextAccessor _httpContext;
+    public Session(IHttpContextAccessor httpContext)
+    {
+        _httpContext = httpContext;
+    }
+
+    public void CreateUserSession(UserModel user)
+    {
+        string valor = JsonSerializer.Serialize(user);
+        //HttpContext.Session.SetString RECEBE um objeto chave valor, sendo que os dois tem que ser do tipo string. Por isso é necessário serializar o user
+        _httpContext.HttpContext.Session.SetString("UserSessionLogin", valor);
+    }
+
+    public UserModel GetUserSession()
+    {
+        string userSession = _httpContext.HttpContext.Session.GetString("UserSessionLogin");
+
+        if (string.IsNullOrEmpty(userSession)) return null;
+
+        return JsonSerializer.Deserialize<UserModel>(userSession);
+    }
+
+    public void RemoveUserSession()
+    {
+        _httpContext.HttpContext.Session.Remove("UserSessionLogin");
+    }
+}
